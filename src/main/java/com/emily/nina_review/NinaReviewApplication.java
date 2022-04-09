6f9740df2ffb2737854c715f9c23275cc5eb08c7
Emily-ting <75919624+emily-ting@users.noreply.github.com>
2022-04-10 01:10:27 +0800

    initializr project

---
+++ src/main/java/com/emily/nina_review/NinaReviewApplication.java
@@ -0,0 +1,13 @@
+package com.emily.nina_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinaReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinaReviewApplication.class, args);
+	}
+
+}
